9a86b7b8a9dada0d3ac1f2f6fcf56bf8e67989da
TutkuKagan <tkbayir03@gmail.com>
2026-05-14 00:55:32 +0300

    Comparison/Config
    Just added the enums and the Configuration part so that we can work on the code better.

--- src/main/java/gradeflow/model/ComparisonMethod.java
+++ src/main/java/gradeflow/model/ComparisonMethod.java
@@ -1,5 +1,9 @@
+
 package gradeflow.model;
 
 public enum ComparisonMethod {
     
+    EXACT,
+    
+    TRIMMED
 }
--- src/main/java/gradeflow/model/Configuration.java
+++ src/main/java/gradeflow/model/Configuration.java
@@ -1,5 +1,75 @@
 package gradeflow.model;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 public class Configuration {
-    
+
+    private int              id;
+    private String           name;
+    private String           language;
+    private String           compilerPath;
+    private String           compilerArgs;
+    private String           sourceFileName;
+    private String           runCommand;
+    private boolean          isCompiled;
+    private ComparisonMethod comparisonMethod;
+
+    public Configuration() {
+        this.isCompiled        = true;
+        this.comparisonMethod  = ComparisonMethod.TRIMMED;
+    }
+
+    public Configuration(String name, String language, String compilerPath,
+                         String compilerArgs, String sourceFileName, String runCommand,
+                         boolean isCompiled, ComparisonMethod comparisonMethod) {
+        this.name             = name;
+        this.language         = language;
+        this.compilerPath     = compilerPath;
+        this.compilerArgs     = compilerArgs;
+        this.sourceFileName   = sourceFileName;
+        this.runCommand       = runCommand;
+        this.isCompiled       = isCompiled;
+        this.comparisonMethod = comparisonMethod;
+    }
+
+   
+    private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+    public String toJson() {
+        return gson.toJson(this);
+    }
+
+    public static Configuration fromJson(String json) {
+        return gson.fromJson(json, Configuration.class);
+    }
+
+    public int              getId()                    { return id; }
+    public void             setId(int id)              { this.id = id; }
+
+    public String           getName()                  { return name; }
+    public void             setName(String n)          { this.name = n; }
+
+    public String           getLanguage()              { return language; }
+    public void             setLanguage(String l)      { this.language = l; }
+
+    public String           getCompilerPath()          { return compilerPath; }
+    public void             setCompilerPath(String p)  { this.compilerPath = p; }
+
+    public String           getCompilerArgs()          { return compilerArgs; }
+    public void             setCompilerArgs(String a)  { this.compilerArgs = a; }
+
+    public String           getSourceFileName()        { return sourceFileName; }
+    public void             setSourceFileName(String s){ this.sourceFileName = s; }
+
+    public String           getRunCommand()            { return runCommand; }
+    public void             setRunCommand(String r)    { this.runCommand = r; }
+
+    public boolean          isCompiled()               { return isCompiled; }
+    public void             setCompiled(boolean c)     { this.isCompiled = c; }
+
+    public ComparisonMethod getComparisonMethod()      { return comparisonMethod; }
+    public void             setComparisonMethod(ComparisonMethod m) { this.comparisonMethod = m; }
+
+    @Override public String toString() { return name + " (" + language + ")"; }
 }
